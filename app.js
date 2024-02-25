f7c401d5720fafa53133a970ce013ffada364c63
danylocrazy <danechkatugolukov@gmail.com>
2024-02-25 02:44:43 +0200

    adaptive_wd

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const input1 = document.getElementById('hryvna');
-const input2 = document.getElementById('dollars');
+const input2 = document.getElementById('exchange');
 const btn = document.getElementById('btn');
 const usd = document.getElementById('usd');
 const welche = document.getElementById('vubor');
@@ -13,13 +13,12 @@ const url = `https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json`;
 fetch(url).then(response => response.json())
 .then(data => {
     dollar = data.find(currency => currency.cc === "USD");
-    console.log(`На ${dollar.exchangedate} курс доллара равняется: ${dollar.rate}`);
     document.getElementById('usd').textContent = dollar.rate;
+
     euro = data.find(currency => currency.cc === "EUR");
-    console.log(`${euro.rate}`);
     document.getElementById('eur').textContent = euro.rate;
+
     zloty = data.find(currency => currency.cc === "PLN");
-    console.log(`${zloty.rate}`);
     document.getElementById('pln').textContent = zloty.rate;
 })
 .catch(error => console.error('close: ' , error));
@@ -27,19 +26,19 @@ fetch(url).then(response => response.json())
 
 btn.onclick = function () { 
     if(vubor.value === "usd"){
-    const res = input1.value / dollar.rate;
-    const newres = res.toFixed(2); 
-    console.log(newres); 
-    document.getElementById('dollars').value = newres;
+        const res = input1.value / dollar.rate;
+        const newres = res.toFixed(2); 
+        console.log(newres); 
+        document.getElementById('exchange').value = newres;
     } else if (vubor.value === "eur") {
         const res = input1.value / euro.rate;
         const newres = res.toFixed(2); 
         console.log(newres); 
-        document.getElementById('dollars').value = newres;
+        document.getElementById('exchange').value = newres;
     } else if (vubor.value === "pln") {
         const res = input1.value / zloty.rate;
         const newres = res.toFixed(2); 
         console.log(newres); 
-        document.getElementById('dollars').value = newres;
+        document.getElementById('exchange').value = newres;
     }
 };
